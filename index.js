bd701a24f0e03c92fef2eeced34b58f47e6039f0
Maya akter <mayaakter1096@gmail.com>
2025-08-04 13:22:28 +0600

    add comments and work on update

--- index.js
+++ index.js
@@ -35,9 +35,34 @@ async function run() {
     try {
 
         await client.connect();
-        
+        // create db
         const recipeCollection = client.db('recipedb').collection('recipes');
 
+        //  all the recipe data from db
+        app.get('/all-recipe', async (req, res) => {
+            const cursor = recipeCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // recipe details 
+        app.get('/recipe-details/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await recipeCollection.findOne(query);
+            res.send(result);
+        });
+
+        // my-recipes from db
+        app.get('/my-recipes/', async (req, res) => {
+            const email = req.query.email;
+
+            const result = await recipeCollection.find({ 'user.email': email }).toArray();
+            res.send(result);
+
+        });
+
+        // save recipe data from client to db
         app.post('/recipe', async (req, res) => {
             const recipe = req.body;
             const result = await recipeCollection.insertOne(recipe);
@@ -45,35 +70,14 @@ async function run() {
 
         });
 
-        app.get('/all-recipe', async (req, res) => {
-            const cursor = recipeCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        });
-
-        app.get('/recipe-details/:id',async(req,res)=>{
+        app.put('/recipe-update/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)};
-            const result = await recipeCollection.findOne(query);
-            res.send(result);
-        });
-
-        app.get('/my-recipes/',async(req,res)=>{
-            const email = req.query.email;
-            
-            const result = await recipeCollection.find({'user.email':email}).toArray();
-            res.send(result);
-            
-        });
-
-        app.put('/recipe-update/:id',async(req,res)=>{
-            const id = req.params.id;
-            const query = {_id:new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const updateRecipe = req.body;
-            const updateDoct={
-                $set:updateRecipe
+            const updatedoct = {
+                $set: updateRecipe
             }
-            const result = await recipeCollection.updateOne(query,updateDoct);
+            const result = await recipeCollection.updateOne(query, updatedoct);
             res.send(result);
         })
 
@@ -82,8 +86,10 @@ async function run() {
 
 
 
+
+
     } finally {
-        
+
         // await client.close();
     }
 }
